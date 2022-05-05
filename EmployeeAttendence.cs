1123ff49af35ee00bce6f771b10211f281f4f8a9
vpradhap <pradhapvenkat@gmail.com>
2022-05-05 23:42:39 +0530

    Refractored : Part Time Employee Wage Added

--- EmployeeAttendence.cs
+++ EmployeeAttendence.cs
@@ -9,18 +9,24 @@ namespace EmployeeWageComputation
     public class EmployeeWage
     {
         int WAGE_PER_HOUR = 20;
-        int FULL_DAY_HOUR = 8; 
+        int FULL_DAY_HOUR = 8;
+        int PART_TIME_HOUUR = 4;
         Random Generate = new Random();
         public void DailyWage()
         {
             int emphrs = 0;
             int dailywage = 0;
-            int empcheck = Generate.Next(0, 2);
+            int empcheck = Generate.Next(0, 3);
 
             if (empcheck == 0)
             {
-                Console.WriteLine("Employee is Present");
+                Console.WriteLine("Employee is Present\nFull Time Employee");
                 emphrs = 8;
+            }
+            else if (empcheck == 1)
+            {
+                Console.WriteLine("Employee is Present\nPart Time Employee");
+                emphrs = 4;
             }
             else
             {
