1b3556c45f20a1a3345b147cbbb9a694e9e8fcef
vpradhap <pradhapvenkat@gmail.com>
2022-05-05 22:36:49 +0530

    Added : Employee Present or Absent

---
+++ EmployeeAttendence.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    public class EmployeeAttendence
+    {
+        Random Generate = new Random();
+        public void CheckPresentOrNot()
+        {
+            int empcheck = Generate.Next(0,2);
+            if (empcheck == 0)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿
+﻿using EmployeeWageComputation;
+
 Console.WriteLine("Welcome to Employee Wage Computation");
+EmployeeAttendence obj = new EmployeeAttendence();
+obj.CheckPresentOrNot();
