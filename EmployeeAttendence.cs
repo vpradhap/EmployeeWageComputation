029559dc3a21be551dde69080054e998b9b2af53
vpradhap <pradhapvenkat@gmail.com>
2022-05-06 11:06:22 +0530

    Added : Calculation of Employee wage for a Month

--- EmployeeAttendence.cs
+++ EmployeeAttendence.cs
@@ -11,32 +11,40 @@ namespace EmployeeWageComputation
         int WAGE_PER_HOUR = 20;
         int FULL_DAY_HOUR = 8;
         int PART_TIME_HOUUR = 4;
+        int WORKING_DAY_PER_MONTH = 20;
         Random Generate = new Random();
         public void DailyWage()
         {
             int emphrs = 0;
             int dailywage = 0;
-            int empcheck = Generate.Next(0, 3);
-
-            switch (empcheck)
+            int monthlywage = 0;
+            int day =0;
+            for  (day=1 ; day <= WORKING_DAY_PER_MONTH; day++)
             {
-                case 0:
-                    Console.WriteLine("Employee is Present\nFull Time Employee");
-                    emphrs = FULL_DAY_HOUR;
-                    break;
+                int empcheck = Generate.Next(0, 3);
 
-                case 1:
-                    Console.WriteLine("Employee is Present\nPart Time Employee");
-                    emphrs = PART_TIME_HOUUR;
-                    break;
+                switch (empcheck)
+                {
+                    case 0:
+                        Console.WriteLine("Employee is Present\nFull Time Employee");
+                        emphrs = FULL_DAY_HOUR;
+                        break;
 
-                default:
-                    Console.WriteLine("Employee is Absent");
-                    emphrs = 0;
-                    break;
+                    case 1:
+                        Console.WriteLine("Employee is Present\nPart Time Employee");
+                        emphrs = PART_TIME_HOUUR;
+                        break;
+
+                    default:
+                        Console.WriteLine("Employee is Absent");
+                        emphrs = 0;
+                        break;
+                }
+                dailywage = emphrs * WAGE_PER_HOUR;
+                Console.WriteLine("Employee Daily Wage : " + dailywage);
+                monthlywage += dailywage; 
             }
-            dailywage = emphrs * WAGE_PER_HOUR;
-            Console.WriteLine("Employee Daily Wage : " + dailywage);
+            Console.WriteLine("\nEmployee Monthly Wage : " +monthlywage);
         }
     }    
 }
--- Program.cs
+++ Program.cs
@@ -3,3 +3,4 @@
 Console.WriteLine("Welcome to Employee Wage Computation");
 EmployeeWage obj = new EmployeeWage();
 obj.DailyWage();
+Console.ReadKey();
